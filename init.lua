0f1a81f637ebf14c94387a1ae5c4097aebb5fc4b
Vladi Skokov <vladi@skokov.net>
2023-10-13 11:17:36 -0400

    latex

--- init.lua
+++ init.lua
@@ -1,2 +1,61 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("luasnip").config.set_config({ -- Setting LuaSnip config
+
+  -- Enable autotriggered snippets
+  enable_autosnippets = true,
+
+  -- Use Tab (or some other key if you prefer) to trigger visual selection
+  store_selection_keys = "<Tab>",
+})
+
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
+
+
+vim.cmd[[
+
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+
+
+"------------------------------------------------------------------------------
+" slime configuration
+"------------------------------------------------------------------------------
+let g:slime_target = 'tmux'
+let g:slime_default_config = {"socket_name": "default", "target_pane": "{right-of}"}
+let g:slime_dont_ask_default = 1
+
+"------------------------------------------------------------------------------
+" julia-cell configuration
+"------------------------------------------------------------------------------
+" Use '##' tags to define cells
+let g:julia_cell_delimit_cells_by = 'tags'
+
+" map <Leader>jr to run entire file
+nnoremap <Leader>jr :JuliaCellRun<CR>
+
+" map <Leader>jc to execute the current cell
+nnoremap <Leader>jc :JuliaCellExecuteCell<CR>
+
+" map <Leader>jC to execute the current cell and jump to the next cell
+nnoremap <Leader>jC :JuliaCellExecuteCellJump<CR>
+
+" map <Leader>jl to clear Julia screen
+nnoremap <Leader>jl :JuliaCellClear<CR>
+
+" map <Leader>jp and <Leader>jn to jump to the previous and next cell header
+nnoremap <Leader>jp :JuliaCellPrevCell<CR>
+nnoremap <Leader>jn :JuliaCellNextCell<CR>
+
+" map <Leader>je to execute the current line or current selection
+nmap <Leader>je <Plug>SlimeLineSend
+xmap <Leader>je <Plug>SlimeRegionSend
+
+" map <F5> to save and run script
+nnoremap <F5> :w<CR>:JuliaCellRun<CR>
+
+]]
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 vim.g.tex_conceal = "abdmg"
 vim.opt.conceallevel = 2
+
+
+vim.g.vimtex_view_method = 'skim' -- Choose which program to use to view PDF file 
+vim.g.vimtex_view_skim_sync = 1 -- Value 1 allows forward search after every successful compilation
+vim.g.vimtex_view_skim_activate = 1
